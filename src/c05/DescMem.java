fa0dc459044561a1dc933ee495ff3247d5b81830
VimIcewind <VimIcewind@gmail.com>
2016-02-03 20:00:54 +0800

    public friendly protected private

---
+++ src/c05/DescMem.java
@@ -0,0 +1,11 @@
+package c05;
+
+/**
+ * Created by HWD on 2016/2/3.
+ */
+public class DescMem {
+    public String publicS = "public";
+    String s = "friendly";
+    protected String protectedS = "protected";
+    private String privateS = "private";
+}
