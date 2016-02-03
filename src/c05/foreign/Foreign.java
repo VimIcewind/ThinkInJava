502e31b9481140f20c95d02338cfbecdb0748d49
VimIcewind <VimIcewind@gmail.com>
2016-02-03 17:23:07 +0800

    PackageClass

---
+++ src/c05/foreign/Foreign.java
@@ -0,0 +1,10 @@
+//: Foreign.java
+package c05.foreign;
+
+import c05.PackagedClass;
+
+public class Foreign {
+    public static void main(String[] args) {
+        PackagedClass pc = new PackagedClass();
+    }
+} ///:~
