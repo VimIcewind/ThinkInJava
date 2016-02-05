daa962115fac2aa8f7cfc554c6842f8eb656ded1
VimIcewind <VimIcewind@gmail.com>
2016-02-05 16:09:59 +0800

    inheritance

---
+++ src/c06/Cartoon.java
@@ -0,0 +1,25 @@
+//: Cartoon.java
+//// Constructor calls during inheritance
+package c06;
+
+class Art {
+    Art() {
+        System.out.println("Art constructor");
+    }
+}
+
+class Drawing extends Art {
+    Drawing() {
+        System.out.println("Drawing constructor");
+    }
+}
+
+public class Cartoon extends Drawing {
+    Cartoon() {
+        System.out.println("Cartoon constructor");
+    }
+
+    public static void main(String[] args) {
+        Cartoon x = new Cartoon();
+    }
+} ///:~
