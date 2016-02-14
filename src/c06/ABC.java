27529c7eea00e0ee659e7371af2178cb58fbd16f
VimIcewind <VimIcewind@gmail.com>
2016-02-14 11:15:16 +0800

    The full process of initialization exercise.

--- src/c06/ABC.java
+++ src/c06/ABC.java
@@ -8,20 +8,39 @@ class A extends C {
     public A() {
         System.out.println("A()");
     }
+
+    public A(String s) {
+        System.out.println("A(" + s + ")");
+    }
 }
 
 class B {
     public B() {
         System.out.println("B()");
     }
+
+    public B(String s) {
+        System.out.println("B(" + s + ")");
+    }
 }
 
 class C {
+
     B b = new B();
+
+    public C() {
+
+    }
+
+    public C(String s) {
+        A a = new A(s);
+        b = new B(s);
+    }
 }
 
 public class ABC {
     public static void main(String[] args) {
         C c = new C();
+        C cs = new C("Called by C");
     }
 }
