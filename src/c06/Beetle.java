2b358e4f4df6382df6efd17c1258f21d54c579b4
VimIcewind <VimIcewind@gmail.com>
2016-02-14 10:09:06 +0800

    The full process of initialization.

---
+++ src/c06/Beetle.java
@@ -0,0 +1,42 @@
+//: Beetle.java
+// The full process of initialization
+package c06;
+
+class Insect {
+    int i = 9;
+    int j;
+    int k = prt("Insect.k initialized");
+
+    Insect() {
+        prt("i = " + i + ", j = " + j);
+        j = 39;
+    }
+
+    static int x1 = prt("static Insect.x1 initialized");
+
+    static int prt(String s) {
+        System.out.println(s);
+        return 47;
+    }
+}
+
+public class Beetle extends Insect {
+    int k = prt("Beetle.k initialized");
+
+    Beetle() {
+        prt("k = " + k);
+        prt("j = " + j);
+    }
+
+    static int x2 = prt("static Beetle.x2 initialized");
+
+    static int prt(String s) {
+        System.out.println(s);
+        return 63;
+    }
+
+    public static void main(String[] args) {
+        prt("Beetle constructor");
+        Beetle b = new Beetle();
+    }
+} ///:~
