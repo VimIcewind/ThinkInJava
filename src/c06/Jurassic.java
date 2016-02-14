81df61c3e35dcdec1adb15835f741e99a402d078
VimIcewind <VimIcewind@gmail.com>
2016-02-14 09:48:38 +0800

    final class

---
+++ src/c06/Jurassic.java
@@ -0,0 +1,28 @@
+//: Jurassic.java
+// Making an entire class final
+package c06;
+
+class SmallBrain {
+
+}
+
+final class Dinosaur {
+    int i = 7;
+    int j = 1;
+    SmallBrain x = new SmallBrain();
+    void f() {
+
+    }
+}
+
+//! class Further extends Dinosaur {}
+// error Cannot extend final class 'Dinosaur'
+
+public class Jurassic {
+    public static void main(String[] args) {
+        Dinosaur n = new Dinosaur();
+        n.f();
+        n.i = 40;
+        n.j++;
+    }
+} ///:~
