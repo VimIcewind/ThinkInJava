33210bea23f09aa0dade36352070323ad9d3345b
VimIcewind <VimIcewind@gmail.com>
2016-02-16 16:34:28 +0800

    SimpleEnumUse

---
+++ src/c05/SimpleEnumUse.java
@@ -0,0 +1,12 @@
+//: SimpleEnumUse.java
+package c05;
+
+public class SimpleEnumUse {
+    public static void main(String[] args) {
+        Spiciness howHot = Spiciness.MEDIUM;
+        System.out.println(howHot);
+    }
+}
+/* Output
+MEDIUM
+ *///:~
---
+++ src/c05/Spiciness.java
@@ -0,0 +1,6 @@
+//: Spiciness.java
+package c05;
+
+public enum Spiciness {
+    NOT, MILD, MEDIUM, HOT, FLAMING
+} ///:~
