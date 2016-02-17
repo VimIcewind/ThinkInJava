ad6a4aac6933d2eb1db8f187e0c6f1765f83956d
VimIcewind <VimIcewind@gmail.com>
2016-02-17 16:48:30 +0800

    RandomDoubles

---
+++ src/C09/RandomDoubles.java
@@ -0,0 +1,19 @@
+//: RandomDoubles.java
+package C09;
+
+import java.util.*;
+
+public class RandomDoubles {
+    private static Random rand = new Random(47);
+
+    public double next() {
+        return rand.nextDouble();
+    }
+
+    public static void main(String[] args) {
+        RandomDoubles rd = new RandomDoubles();
+        for (int i = 0; i < 7; i++) {
+            System.out.println(rd.next() + " ");
+        }
+    }
+}
