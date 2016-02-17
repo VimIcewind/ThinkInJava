c20f74a014e902cd71de08d0a32224c30b885da1
VimIcewind <VimIcewind@gmail.com>
2016-02-17 10:34:20 +0800

    Process interface version.

---
+++ src/C09/interfaceprocessor/Apply.java
@@ -0,0 +1,9 @@
+// interfaceprocessor/Apply.java
+package C09.interfaceprocessor;
+
+public class Apply {
+    public static void process(Processor p, Object s) {
+        System.out.println("Using Processor " + p.name());
+        System.out.println(p.process(s));
+    }
+} ///:~
