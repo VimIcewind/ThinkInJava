8dee2b733fde4ea438611159c9138404e5355c06
VimIcewind <VimIcewind@gmail.com>
2016-02-19 10:26:53 +0800

    Nested classes (static inner classes).

---
+++ src/c10/Parcel11.java
@@ -0,0 +1,52 @@
+// Parcel11.java
+// Nested classes (static inner classes).
+package c10;
+
+public class Parcel11 {
+    private static class ParcelContents implements Contents {
+        private int i = 11;
+
+        public int value() {
+            return i;
+        }
+    }
+
+    protected static class ParcelDestination implements Destination {
+        private String label;
+
+        private ParcelDestination(String whereTo) {
+            label = whereTo;
+        }
+
+        public String readLabel() {
+            return label;
+        }
+
+        // Nested classes can contain other static elemetns:
+        public static void f() {
+
+        }
+
+        static int x = 10;
+
+        static class AnotherLevel {
+            public static void f() {
+
+            }
+            static int x = 10;
+        }
+    }
+
+    public static Destination destination(String s) {
+        return new ParcelDestination(s);
+    }
+
+    public static Contents contents() {
+        return new ParcelContents();
+    }
+
+    public static void main(String[] args) {
+        Contents c = contents();
+        Destination d = destination("Tasmania");
+    }
+} ///:~
