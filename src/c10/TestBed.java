a6d25085c687c1cc8e54d24400f73ae11496c8f7
VimIcewind <VimIcewind@gmail.com>
2016-02-20 17:42:39 +0800

    Putting test code in a nested class.

---
+++ src/c10/TestBed.java
@@ -0,0 +1,16 @@
+// TestBed.java
+// Putting test code in a nested class.
+package c10;
+
+public class TestBed {
+    public void f() {
+        System.out.println("f()");
+    }
+
+    public static class Tester {
+        public static void main(String[] args) {
+            TestBed t = new TestBed();
+            t.f();
+        }
+    }
+}
