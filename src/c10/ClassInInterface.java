79a33b5b7ce1e319c980536fd63f2a3d525ea4c8
VimIcewind <VimIcewind@gmail.com>
2016-02-20 17:42:14 +0800

    ClassInInterface

---
+++ src/c10/ClassInInterface.java
@@ -0,0 +1,17 @@
+//: ClassInInterface.java
+// {main: ClassInInterface$Test}
+package c10;
+
+public interface ClassInInterface {
+    void howdy();
+
+    class Test implements ClassInInterface {
+        public void howdy() {
+            System.out.println("Howdy!");
+        }
+
+        public static void main(String[] args) {
+            new Test().howdy();
+        }
+    }
+}
