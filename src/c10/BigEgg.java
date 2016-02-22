e5c7080932834c424599a8cb6e7a26c6bcf0a385
VimIcewind <VimIcewind@gmail.com>
2016-02-22 14:58:33 +0800

    An inner class cannot be overriden like a method.

---
+++ src/c10/BigEgg.java
@@ -0,0 +1,34 @@
+//: BigEgg.java
+// An inner class cannot be overriden like a method.
+package c10;
+
+class Egg {
+    private Yolk y;
+
+    protected class Yolk {
+        public Yolk() {
+            System.out.println("Egg.Yolk()");
+        }
+    }
+
+    public Egg() {
+        System.out.println("New Egg()");
+        y = new Yolk();
+    }
+}
+
+public class BigEgg extends Egg {
+    public class Yolk {
+        public Yolk() {
+            System.out.println("BigEgg.Yolk()");
+        }
+    }
+
+    public static void main(String[] args) {
+        new BigEgg();
+    }
+}
+/* Output:
+New Egg()
+Egg.Yolk()
+ *///:~
