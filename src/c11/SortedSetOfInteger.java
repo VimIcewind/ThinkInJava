5bbd06a9ccc667e8313e2577d608fab8bdfc1928
VimIcewind <VimIcewind@gmail.com>
2016-02-25 20:19:20 +0800

    SetOf Integer

--- src/c11/SortedSetOfInteger.java
+++ src/c11/SortedSetOfInteger.java
@@ -1,12 +1,12 @@
-// SetOfInteger.java
+// SortedSetOfInteger.java
 package c11;
 
 import java.util.*;
 
-public class SetOfinteger {
+public class SortedSetOfInteger {
     public static void main(String[] args) {
         Random rand = new Random(47);
-        Set<Integer> intset = new HashSet<Integer>();
+        SortedSet<Integer> intset = new TreeSet<Integer>();
         for (int i = 0; i < 10000; i++) {
             intset.add(rand.nextInt(30));
         }
