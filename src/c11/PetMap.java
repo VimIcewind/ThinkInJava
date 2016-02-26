c864ddb9edf6f1723189053fe9463b47c14229d9
VimIcewind <VimIcewind@gmail.com>
2016-02-26 10:47:13 +0800

    PetMap

---
+++ src/c11/PetMap.java
@@ -0,0 +1,19 @@
+//: PetMap.java
+package c11;
+
+import c14.pets.*;
+import java.util.*;
+
+public class PetMap {
+    public static void main(String[] args) {
+        Map<String, Pet> petMap = new HashMap<String, Pet>();
+        petMap.put("My Cat", new Cat("Molly"));
+        petMap.put("My Dog", new Dog("Ginger"));
+        petMap.put("My Hamster", new Hamster("Bosco"));
+        System.out.println(petMap);
+        Pet dog = petMap.get("My Dog");
+        System.out.println(dog);
+        System.out.println(petMap.containsKey("My Dog"));
+        System.out.println(petMap.containsValue(dog));
+    }
+}
