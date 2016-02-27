98348cfb5dcfb4dd6fae08f061a61d8a8b60362e
VimIcewind <VimIcewind@gmail.com>
2016-02-27 15:52:21 +0800

    Logging caught exceptions.

---
+++ src/c12/LoggingExceptions2.java
@@ -0,0 +1,24 @@
+// LoggingExceptions2.java
+// Logging caught exceptions.
+package c12;
+
+import java.util.logging.*;
+import java.io.*;
+
+public class LoggingExceptions2 {
+    private static Logger logger = Logger.getLogger("LoggingExceptions2");
+
+    static void logException(Exception e) {
+        StringWriter trace = new StringWriter();
+        e.printStackTrace(new PrintWriter(trace));
+        logger.severe(trace.toString());
+    }
+
+    public static void main(String[] args) {
+        try {
+            throw new NullPointerException();
+        } catch(NullPointerException e) {
+            logException(e);
+        }
+    }
+}
