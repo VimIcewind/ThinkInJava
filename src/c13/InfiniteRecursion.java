91e8d45a64b662c33521698a20bc3de382d32f20
VimIcewind <VimIcewind@gmail.com>
2016-03-03 21:49:06 +0800

    Using super.toStrign version.

--- src/c13/InfiniteRecursion.java
+++ src/c13/InfiniteRecursion.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class InfiniteRecursion {
     public String toString() {
-        return " InfiniteRecursion address: " + this + "\n";
+        return "InfiniteRecursion address: " + super.toString() + "\n";
     }
 
     public static void main(String[] args) {
