803ec6e8893bf935407aa68644f22fde8cb36ebc
VimIcewind <VimIcewind@gmail.com>
2016-03-03 16:05:17 +0800

    Concatenation

---
+++ src/c13/Concatenation.java
@@ -0,0 +1,10 @@
+//: Concatenation.java
+package c13;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        String mango = "mango";
+        String s = "abc" + mango + "def" + 47;
+        System.out.println(s);
+    }
+}
