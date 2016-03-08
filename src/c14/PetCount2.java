d9251e14b388d20d8c7f68013674cbd005a09ee4
VimIcewind <VimIcewind@gmail.com>
2016-03-08 14:35:04 +0800

    PetCount2

---
+++ src/c14/PetCount2.java
@@ -0,0 +1,10 @@
+//: PetCount2.java
+package c14;
+
+import c14.pets.*;
+
+public class PetCount2 {
+    public static void main(String[] args) {
+        PetCount.countPets(Pets.creator);
+    }
+}
