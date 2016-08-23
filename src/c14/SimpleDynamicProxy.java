8b1839d358bc16e45666f2efeabfbcde490e25c7
VimIcewind <VimIcewind@gmail.com>
2016-08-23 17:25:51 +0800

    SimpleDynamicProxy

---
+++ src/c14/SimpleDynamicProxy.java
@@ -0,0 +1,38 @@
+package c14;
+
+import java.lang.reflect.*;
+
+class DynamicProxyHandler implements InvocationHandler {
+    private Object proxied;
+
+    public DynamicProxyHandler(Object proxied) {
+        this.proxied = proxied;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("**** proxy: " + proxy.getClass() +
+                ", method: " + method + ", args: " + args);
+        if (args != null)
+            for (Object arg : args)
+                System.out.println(" " + arg);
+        return method.invoke(proxied, args);
+    }
+}
+
+class SimpleDynamicProxy {
+    public static void consumer(Interface iface) {
+        iface.doSomething();
+        iface.somethingElse("bonobo");
+    }
+
+    public static void main(String[] args) {
+        RealObject real = new RealObject();
+        consumer(real);
+        // Insert a proxy and call again:
+        Interface proxy = (Interface) Proxy.newProxyInstance(
+                Interface.class.getClassLoader(),
+                new Class[] { Interface.class },
+                new DynamicProxyHandler(real));
+        consumer(proxy);
+    }
+}
