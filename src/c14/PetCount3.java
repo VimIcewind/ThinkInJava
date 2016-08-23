cbc7037923f489550984e8efc53b3af2a16673dd
VimIcewind <VimIcewind@gmail.com>
2016-08-23 14:35:13 +0800

    PetCounter3

---
+++ src/c14/PetCount3.java
@@ -0,0 +1,48 @@
+//: PetCount3.java
+// Using inInstance()
+package c14;
+
+import c14.pets.*;
+import net.mindview.util.*;
+
+import java.util.*;
+
+public class PetCount3 {
+    static class PetCounter extends LinkedHashMap<Class<? extends Pet>, Integer> {
+        public PetCounter() {
+            super(MapData.map(LiteralPetCreator.allTypes, 0));
+        }
+
+        public void count(Pet pet) {
+            // Class.isInstance() eliminates instanceofs:
+            for (Map.Entry<Class<? extends Pet>, Integer> pair : entrySet()) {
+                if (pair.getKey().isInstance(pet)) {
+                    put(pair.getKey(), pair.getValue() + 1);
+                }
+            }
+        }
+
+        public String toString() {
+            StringBuilder result = new StringBuilder("{");
+            for (Map.Entry<Class<? extends Pet>, Integer> pair : entrySet()) {
+                result.append(pair.getKey().getSimpleName());
+                result.append("=");
+                result.append(pair.getValue());
+                result.append(", ");
+            }
+            result.delete(result.length() - 2, result.length());
+            result.append("}");
+            return result.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        PetCounter petCount = new PetCounter();
+        for (Pet pet : Pets.createArray(20)) {
+            System.out.print(pet.getClass().getSimpleName() + " ");
+            petCount.count(pet);
+        }
+        System.out.println();
+        System.out.println(petCount);
+    }
+}
