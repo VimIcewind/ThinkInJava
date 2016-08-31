f7ba4613ca007f1a01661b4fb55b053ecdb955f4
VimIcewind <VimIcewind@gmail.com>
2016-08-31 16:05:38 +0800

    BasicGenerator

---
+++ src/c15/BasicGeneratorDemo.java
@@ -0,0 +1,12 @@
+package c15;
+
+import net.mindview.util.Generator;
+
+public class BasicGeneratorDemo {
+    public static void main(String[] args) {
+        Generator<CountedObject> gen = BasicGenerator.create(CountedObject.class);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(gen.next());
+        }
+    }
+}
