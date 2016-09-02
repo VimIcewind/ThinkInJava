78fe2d22eb1a2c7dd8a16a1ed10c7dda36e622d4
VimIcewind <VimIcewind@gmail.com>
2016-09-02 16:58:53 +0800

    ListMaker

---
+++ src/c15/ListMaker.java
@@ -0,0 +1,14 @@
+package c15;
+
+import java.util.*;
+
+public class ListMaker<T> {
+    List<T> create() {
+        return new ArrayList<T>();
+    }
+
+    public static void main(String[] args) {
+        ListMaker<String> stringMaker = new ListMaker<String>();
+        List<String> stringList = stringMaker.create();
+    }
+}
