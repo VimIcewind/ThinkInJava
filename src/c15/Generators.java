3d1b7e9773335b5b554b4579c90c5cb3c7665e62
VimIcewind <VimIcewind@gmail.com>
2016-09-02 13:36:45 +0800

    BankTeller

---
+++ src/c15/Generators.java
@@ -0,0 +1,29 @@
+package c15;
+
+import c15.coffee.Coffee;
+import c15.coffee.CoffeeGenerator;
+import net.mindview.util.*;
+
+import java.util.*;
+
+public class Generators {
+    public static <T> Collection<T> fill(Collection<T> coll, Generator<T> gen, int n) {
+        for (int i = 0; i < n; i++) {
+            coll.add(gen.next());
+        }
+        return coll;
+    }
+
+    public static void main(String[] args) {
+        Collection<Coffee> coffee = fill(new ArrayList<Coffee>(), new CoffeeGenerator(), 4);
+
+        for (Coffee c : coffee) {
+            System.out.println(c);
+        }
+
+        Collection<Integer> fnumbers = fill(new ArrayList<Integer>(), new Fibonacci(), 12);
+        for (int i : fnumbers) {
+            System.out.print(i + ", ");
+        }
+    }
+}
