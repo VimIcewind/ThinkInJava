54f7d95b81045e1a86c0cec81261a858d56a7bf1
VimIcewind <VimIcewind@gmail.com>
2016-09-02 16:15:30 +0800

    ReturnGenericType

---
+++ src/c15/ReturnGenericType.java
@@ -0,0 +1,13 @@
+package c15;
+
+public class ReturnGenericType<T extends HasF> {
+    private T obj;
+
+    public ReturnGenericType(T x) {
+        obj = x;
+    }
+
+    public T get() {
+        return obj;
+    }
+}
