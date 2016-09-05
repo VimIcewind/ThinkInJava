084042d592dd6cbdb74d9ae92a253b7e7adfe722
VimIcewind <VimIcewind@gmail.com>
2016-09-05 10:38:31 +0800

    CreatorGeneric

---
+++ src/c15/CreatorGeneric.java
@@ -0,0 +1,32 @@
+package c15;
+
+abstract class GenericWithCreate<T> {
+    final T element;
+
+    public GenericWithCreate() {
+        element = create();
+    }
+
+    abstract T create();
+}
+
+class X {
+
+}
+
+class Creator extends GenericWithCreate<X> {
+    X create() {
+        return new X();
+    }
+
+    void f() {
+        System.out.println(element.getClass().getSimpleName());
+    }
+}
+
+public class CreatorGeneric {
+    public static void main(String[] args) {
+        Creator c = new Creator();
+        c.f();
+    }
+}
