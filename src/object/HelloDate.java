f4245cfe000d45d9432cd1812da75181d7cd6c76
VimIcewind <VimIcewind@gmail.com>
2018-05-23 21:05:08 +0800

    HelloDate

---
+++ src/object/HelloDate.java
@@ -0,0 +1,13 @@
+package object;
+
+import java.util.*;
+
+/**
+ * Created by HWD on 2018-5-23.
+ */
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+}
