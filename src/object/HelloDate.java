bf5514672c652a22c45987af65e82abdac493e04
VimIcewind <VimIcewind@gmail.com>
2018-05-24 20:14:44 +0800

    Documentation HelloDate

--- src/object/HelloDate.java
+++ src/object/HelloDate.java
@@ -1,13 +1,25 @@
+// object/HelloDate.java
 package object;
 
 import java.util.*;
 
-/**
- * Created by HWD on 2018-5-23.
+/** The first Thinking in Java example program.
+ * Displays a string and today's date.
+ * @author Bruce Eckel
+ * @author www.MindView.net
+ * @version 4.0
  */
 public class HelloDate {
+    /** Entry point to class & application.
+     * @param args array of string arguments
+     * @throws exceptions No exceptions thrown
+     */
     public static void main(String[] args) {
-        System.out.println("Hello, it's: ");
+        System.out.println("Hello, is's: ");
         System.out.println(new Date());
     }
-}
+} /* Output: (55% match)
+Hello, it's:
+Wed Oct 05 14:39:36 MDT 2005
+*///:~
+
