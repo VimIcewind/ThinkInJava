ab7396d4c7a062c0d1bb44483d446f781ca20f6c
VimIcewind <VimIcewind@gmail.com>
2018-05-28 20:09:16 +0800

    Hello, World!

---
+++ src/object/HelloWorld.java
@@ -0,0 +1,14 @@
+// TIJ4 Chapter Object, Exercise 2, page 89
+// object/HelloWorld.java
+// Follow the HelloDate.java example in this chapter, create a "hello, world"
+// program that simply displays that statement.
+package object;
+
+/**
+ * Created by HWD on 2018-5-28.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
