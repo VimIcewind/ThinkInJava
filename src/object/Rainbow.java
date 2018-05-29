a7bd9cf0614b320d30fee296dc450b95a0f495d5
VimIcewind <VimIcewind@gmail.com>
2018-05-29 20:51:38 +0800

    Rainbow

---
+++ src/object/Rainbow.java
@@ -0,0 +1,31 @@
+// object/Rainbow.java
+// TIJ4 Chapter Object, Exercise 11, page 90
+// Turn the AllColorsOfTheRainbow into a program that compiles and runs.
+package object;
+
+/**
+ * Created by HWD on 2018-5-29.
+ */
+public class Rainbow {
+    public static void main(String[] args) {
+        AllTheColorsOfTheRainbow atc = new AllTheColorsOfTheRainbow();
+        System.out.println("atc.anIntegerRepresentingColors = " + atc.anIntegerRepresentingColors);
+        atc.changeColor(7);
+        atc.changeTheHueOfTheColor(77);
+        System.out.println("After color change, atc.anIntegerRepresentingColors = " + atc.anIntegerRepresentingColors);
+        System.out.println("atc.hue = " + atc.hue);
+    }
+}
+
+class AllTheColorsOfTheRainbow {
+    int anIntegerRepresentingColors = 0;
+    int hue = 0;
+
+    void changeTheHueOfTheColor(int newHue) {
+        hue = newHue;
+    }
+
+    int changeColor(int newColor) {
+        return anIntegerRepresentingColors = newColor;
+    }
+}
