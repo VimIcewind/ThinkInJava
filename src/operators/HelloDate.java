25e3c8d1c31e879ddbe530b66e8b0cc300d50d5a
VimIcewind <VimIcewind@gmail.com>
2018-05-30 20:32:31 +0800

    HelloDate with print

---
+++ src/operators/HelloDate.java
@@ -0,0 +1,18 @@
+//: operators/HelloDate.java
+package operators;
+
+import java.util.*;
+import static net.mindview.util.Print.*;
+
+/**
+ * Created by HWD on 2018-5-30.
+ */
+public class HelloDate {
+    public static void main(String[] args) {
+        print("Hello, it's: ");
+        print(new Date());
+    }
+} /* Output: (55% match)
+Hello, it's:
+Wed May 30 20:30:11 CST 2018
+*///:~
