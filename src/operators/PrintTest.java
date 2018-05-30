d1b40f0efc3864e2df3458ad5c54829bbe003b76
VimIcewind <VimIcewind@gmail.com>
2018-05-30 20:45:24 +0800

    PrintTest

---
+++ src/operators/PrintTest.java
@@ -0,0 +1,13 @@
+package operators;
+
+import static net.mindview.util.Print.*;
+
+/**
+ * Created by HWD on 2018-5-30.
+ */
+public class PrintTest {
+    public static void main(String[] args) {
+        System.out.println("System.out.println()");
+        print("print()");
+    }
+}
