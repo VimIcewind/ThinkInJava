e3023f973c8e4a4b0f9f2c435471d4dc8939839a
VimIcewind <VimIcewind@gmail.com>
2018-06-02 21:44:46 +0800

    Equals

---
+++ src/operators/EqualsMethod.java
@@ -0,0 +1,15 @@
+//: operators/EqualsMethod.java
+package operators;
+
+/**
+ * Created by HWD on 2018-6-2.
+ */
+public class EqualsMethod {
+    public static void main(String[] args) {
+        Integer n1 = new Integer(47);
+        Integer n2 = new Integer(47);
+        System.out.println(n1.equals(n2));
+    }
+} /* Output:
+true
+*///:~
---
+++ src/operators/Equivalence.java
@@ -0,0 +1,17 @@
+//: operators/Equivalence.java
+package operators;
+
+/**
+ * Created by HWD on 2018-6-2.
+ */
+public class Equivalence {
+    public static void main(String[] args) {
+        Integer n1 = new Integer(47);
+        Integer n2 = new Integer(47);
+        System.out.println(n1 == n2);
+        System.out.println(n1 != n2);
+    }
+} /* Output:
+false
+true
+*///:~
