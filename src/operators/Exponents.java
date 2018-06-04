64d64ff04f192722c1d538cedaeb6409e524a6bc
VimIcewind <VimIcewind@gmail.com>
2018-06-04 22:22:03 +0800

    Exponents

---
+++ src/operators/Exponents.java
@@ -0,0 +1,21 @@
+//: operators/Exponents.java
+// "e" means "10 to the power."
+package operators;
+
+/**
+ * Created by HWD on 2018-6-4.
+ */
+public class Exponents {
+    public static void main(String[] args) {
+        // Uppercase and lowercase 'e' are the same:
+        float expFloat = 1.39e-43f;
+        expFloat = 1.39E-43f;
+        System.out.println(expFloat);
+        double expDouble = 47e47d; // 'd' is optional
+        double expDouble2 = 47e47; // Automatically double
+        System.out.println(expDouble);
+    }
+} /* OutPut:
+1.39E-43
+4.7E48
+*///:~
