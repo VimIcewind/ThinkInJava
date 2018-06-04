a25971297f38592a74e6d247f0c4069d790eb34e
VimIcewind <VimIcewind@gmail.com>
2018-06-04 22:09:52 +0800

    LongValues

---
+++ src/operators/LongValues.java
@@ -0,0 +1,19 @@
+// operators/LongValues.java
+// TIJ4 Chapter Operators, Exercise 8, page 109
+// Show that hex and octal notations work with long values.
+// Use Long.toBinaryString to display the results.
+package operators;
+
+import static net.mindview.util.Print.*;
+
+/**
+ * Created by HWD on 2018-6-4.
+ */
+public class LongValues {
+    public static void main(String[] args) {
+        long n1 = 0xffff; // hexadecimal
+        long n2 = 077777; // octal
+        print("long n1 in hex = " + Long.toBinaryString(n1));
+        print("long n2 in oct = " + Long.toBinaryString(n2));
+    }
+}
