a40b01733c9da4fd658dc7a10230d38a1d9c1dc9
VimIcewind <VimIcewind@gmail.com>
2018-06-07 22:08:07 +0800

    RoundingNumbers

---
+++ src/operators/RoundingNumbers.java
@@ -0,0 +1,24 @@
+//: operators/RoundingNumbers.java
+// Rounding floats and doubles.
+package operators;
+
+import static net.mindview.util.Print.*;
+
+/**
+ * Created by HWD on 2018-6-7.
+ */
+public class RoundingNumbers {
+    public static void main(String[] args) {
+        double above = 0.7, below = 0.4;
+        float fabove = 0.7f, fbelow = 0.4f;
+        print("Math.round(above): " + Math.round(above));
+        print("Math.round(below): " + Math.round(below));
+        print("Math.round(fabove): " + Math.round(fabove));
+        print("Math.round(fbelow): " + Math.round(fbelow));
+    }
+} /* Output:
+Math.round(above): 1
+Math.round(below): 0
+Math.round(fabove): 1
+Math.round(fbelow): 0
+*///:~
