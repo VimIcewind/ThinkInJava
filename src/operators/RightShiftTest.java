d3f11a072508cbf269e3790cbc079ff3aa63d2ba
VimIcewind <VimIcewind@gmail.com>
2018-06-07 21:05:57 +0800

    RightShiftTest

---
+++ src/operators/RightShiftTest.java
@@ -0,0 +1,22 @@
+// operators/RightShiftTest.java
+// TIJ4 Chapter Operators, Exercise 11, page 116
+// Start with number that has a binary one in the most significant position
+// (hint: Use a hexadecimal constant). Using the signed right-shift operator,
+// right shift it all the way through all of its binary positions each time
+// displaying the result using Integer.toBinaryString().
+
+package operators;
+
+/**
+ * Created by HWD on 2018-6-7.
+ */
+public class RightShiftTest {
+    public static void main(String[] args) {
+        int h = 0x10000000;
+        System.out.println(Integer.toBinaryString(h));
+        for (int i = 0; i < 28; i++) {
+            h >>>= 1;
+            System.out.println(Integer.toBinaryString(h));
+        }
+    }
+}
