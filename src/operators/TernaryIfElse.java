f057d6a96b76ea31f26ae2e6d3f44eba1e0637a3
VimIcewind <VimIcewind@gmail.com>
2018-06-07 21:25:13 +0800

    TernaryIfElse

---
+++ src/operators/TernaryIfElse.java
@@ -0,0 +1,34 @@
+//: operators/TernaryIfElse.java
+package operators;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * Created by HWD on 2018-6-7.
+ */
+public class TernaryIfElse {
+    static int ternary(int i) {
+        return i < 10 ? i * 100 : i * 10;
+    }
+
+    static int standardIfElse(int i) {
+        if (i < 10) {
+            return i * 100;
+        }
+        else {
+            return i * 10;
+        }
+    }
+
+    public static void main(String[] args) {
+        print(ternary(9));
+        print(ternary(10));
+        print(standardIfElse(9));
+        print(standardIfElse(10));
+    }
+} /* Output:
+900
+100
+900
+100
+*///:~
