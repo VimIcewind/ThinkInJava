2ed97e6ee23d2fd6eccb5e3437ee3ba7a11a7814
VimIcewind <VimIcewind@gmail.com>
2018-06-12 21:01:31 +0800

    Count

---
+++ src/control/Count.java
@@ -0,0 +1,15 @@
+// control/Count.java
+// TIJ Chapter Control, Exercise 1, page 139
+// Write a program that prints values from 1 to 100
+package control;
+
+/**
+ * Created by HWD on 2018-6-12.
+ */
+public class Count {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
