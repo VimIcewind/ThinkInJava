58e366aa1165a3424f9bec337947f4f18d560c8c
VimIcewind <VimIcewind@gmail.com>
2018-06-19 21:36:26 +0800

    ForEachString

---
+++ src/control/ForEachString.java
@@ -0,0 +1,15 @@
+//: control/ForEachString.java
+package control;
+
+/**
+ * Created by HWD on 2018-6-19.
+ */
+public class ForEachString {
+    public static void main(String[] args) {
+        for (char c : "An African Swallow".toCharArray()) {
+            System.out.print(c + " ");
+        }
+    }
+} /* Output:
+A n   A f r i c a n   S w a l l o w
+*///:~
