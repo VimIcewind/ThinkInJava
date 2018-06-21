b05eeb319bcf4858d9414603406852e423b54d67
VimIcewind <VimIcewind@gmail.com>
2018-06-21 22:17:35 +0800

    LabeledWhile

---
+++ src/control/LabeledWhile.java
@@ -0,0 +1,54 @@
+//: control/LabeledWhile.java
+// While loops with "labedled break" and "labeled continue."
+package control;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * Created by HWD on 2018-6-21.
+ */
+public class LabeledWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        outer:
+        while (true) {
+            print("Outer while loop");
+            while (true) {
+                i++;
+                print("i = " + i);
+                if (i == 1) {
+                    print("continue");
+                    continue;
+                }
+                if (i == 3) {
+                    print("continue outer");
+                    continue outer;
+                }
+                if (i == 5) {
+                    print("break");
+                    break;
+                }
+                if (i == 7) {
+                    print("break outer");
+                    break outer;
+                }
+            }
+        }
+    }
+} /* Output
+Outer while loop
+i = 1
+continue
+i = 2
+i = 3
+continue outer
+Outer while loop
+i = 4
+i = 5
+break
+Outer while loop
+i = 6
+i = 7
+break outer
+*///:~
+
