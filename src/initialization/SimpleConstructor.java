dd55b04e6e82d1e46a878ddff003ce6749271629
VimIcewind <VimIcewind@gmail.com>
2018-07-02 21:22:04 +0800

    SimpleConstructor

---
+++ src/initialization/SimpleConstructor.java
@@ -0,0 +1,21 @@
+//: initialization/SimpleConstructor.java
+// Demonstration of a simple constructor.
+package initialization;
+
+/**
+ * Created by HWD on 2018-7-2.
+ */
+class Rock {
+    Rock() { // This is the constructor
+        System.out.print("Rock ");
+    }
+}
+public class SimpleConstructor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Rock();
+        }
+    }
+} /* Output:
+Rock Rock Rock Rock Rock Rock Rock Rock Rock Rock
+*///:~
