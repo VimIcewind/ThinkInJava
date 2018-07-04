c1e012bb420a4b44c48542341e14bfa9e92a0f4b
VimIcewind <VimIcewind@gmail.com>
2018-07-04 21:31:40 +0800

    Apricot

---
+++ src/initialization/Apricot.java
@@ -0,0 +1,16 @@
+//: initialization/Apricot.java
+package initialization;
+
+/**
+ * Created by HWD on 2018-7-4.
+ */
+public class Apricot {
+    void pick() {
+        /* ... */
+    }
+
+    void pit() {
+        pick();
+        /* ... */
+    }
+}
