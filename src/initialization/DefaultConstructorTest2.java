22f72ba4154b208764eca430013623619c8376ca
VimIcewind <VimIcewind@gmail.com>
2018-07-04 11:00:17 +0800

    DefaultConstructorTest2

---
+++ src/initialization/DefaultConstructorTest2.java
@@ -0,0 +1,25 @@
+// initialization/DefaultConstructorTest2.java
+// TIJ4 Chapter Initialization, Exercise 4, page 116
+// Add an overload constructor to the previous exercise that
+// takes a String argument and prints it along with your message.
+package initialization;
+
+/**
+ * Created by HWD on 2018-7-4.
+ */
+class Kabayo2 {
+    Kabayo2() {
+        System.out.println("isa lang kabayo");
+    }
+
+    Kabayo2(String s) {
+        System.out.println(s);
+    }
+}
+
+public class DefaultConstructorTest2 {
+    public static void main(String[] args) {
+        Kabayo2 k = new Kabayo2();
+        Kabayo2 k2 = new Kabayo2("tumutugtog ng gitara");
+    }
+}
