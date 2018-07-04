d539dceaa6ccbb9b359bc7cc0893a29a85dfd763
VimIcewind <VimIcewind@gmail.com>
2018-07-04 10:19:43 +0800

    DefaultConstructor

---
+++ src/initialization/DefaultConstructor.java
@@ -0,0 +1,15 @@
+//: initialization/DefaultConstructor.java
+package initialization;
+
+/**
+ * Created by HWD on 2018-7-4.
+ */
+class Bird {
+
+}
+
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        Bird b = new Bird(); // Default!
+    }
+}///:~
