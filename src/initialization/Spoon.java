b30337886f178efe3493e1fc0fa2f314d93f7121
VimIcewind <VimIcewind@gmail.com>
2018-07-09 11:15:27 +0800

    Spoon

---
+++ src/initialization/Spoon.java
@@ -0,0 +1,12 @@
+//: initialization/Spoon.java
+package initialization;
+
+/**
+ * Created by HWD on 2018-7-9.
+ */
+public class Spoon {
+    static int i;
+    static {
+        i = 47;
+    }
+} ///:~
