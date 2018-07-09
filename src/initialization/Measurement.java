5c07bc3901a248065ab8484a77b15df7e28e3ced
VimIcewind <VimIcewind@gmail.com>
2018-07-09 09:41:30 +0800

    Measurement

---
+++ src/initialization/Measurement.java
@@ -0,0 +1,14 @@
+//: initialization/Measurement.java
+package initialization;
+
+/**
+ * Created by HWD on 2018-7-9.
+ */
+class Depth {
+
+}
+
+public class Measurement {
+    Depth d = new Depth();
+    // ...
+} ///:~
