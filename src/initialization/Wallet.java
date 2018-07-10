ebac7d4a47a3ab705b8782594d54a3d38234a661
VimIcewind <VimIcewind@gmail.com>
2018-07-10 14:25:09 +0800

    Wallet

---
+++ src/initialization/Wallet.java
@@ -0,0 +1,44 @@
+// initialization/Wallet.java
+// TIJ4 Chapter Initialization, Exercise 22, page 140
+// Write a switch statement for the enum in the previous example. For each
+// case, output a description of that particular currency.
+package initialization;
+
+/**
+ * Created by HWD on 2018-7-10.
+ */
+enum Bills {
+    ONE, FIVE, TEN, TWENTY, FIFTY, HUNDRED
+}
+
+public class Wallet {
+    Bills b;
+
+    public static void main(String[] args) {
+        for (Bills b : Bills.values()) {
+            System.out.print("Worth: ");
+            switch (b) {
+            case ONE:
+                System.out.println("$1");
+                break;
+            case FIVE:
+                System.out.println("$5");
+                break;
+            case TEN:
+                System.out.println("$10");
+                break;
+            case TWENTY:
+                System.out.println("$20");
+                break;
+            case FIFTY:
+                System.out.println("$50");
+                break;
+            case HUNDRED:
+                System.out.println("$100");
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
