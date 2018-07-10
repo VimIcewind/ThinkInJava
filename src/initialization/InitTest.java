1382ee984f38a5e4cb169ff94c2c9d95ef941234
VimIcewind <VimIcewind@gmail.com>
2018-07-10 09:58:24 +0800

    InitTest again

--- src/initialization/InitTest.java
+++ src/initialization/InitTest.java
@@ -1,10 +1,10 @@
 // initialization/InitTest.java
-// TIJ4 Chapter Initialization, Exercise 17, page 134
-// Create a class with a constructor that takes a String argument. During
-// constriction, print the argument. Create and array of object references to this
-// class, but don't actually create objects to assign into the array. When you run
-// the program, notice whether the initialization messages from the constructor
-// are printed.
+// TIJ4 Chapter Initialization, Exercise 18, page 134
+// Attach objects to the references in array in exercise 17: Create a class with
+// a constructor that takes a String argument. During constriction, print the
+// argument. Create and array of object references to this class, but don't
+// actually create objects to assign into the array. When you run the program,
+// notice whether the initialization messages from the constructor are printed.
 package initialization;
 
 /**
@@ -21,5 +21,8 @@ class Init{
 public class InitTest {
     public static void main(String[] args) {
         Init[] it = new Init[10];
+        for (int i = 0; i < it.length; i++) {
+            it[i] = new Init(Integer.toString(i));
+        }
     }
 }
