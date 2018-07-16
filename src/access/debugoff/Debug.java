dfeaf50778e5820c39b8f37ee412e830032bb646
VimIcewind <VimIcewind@gmail.com>
2018-07-16 16:19:24 +0800

    debug package

---
+++ src/access/debugoff/Debug.java
@@ -0,0 +1,15 @@
+// access/debug/Debug.java
+// TIJ4 Chapter Access, Exercise 3, page 148
+// Create two packages: debug and debugoff, containing an identical class with a
+// debug() method. The first version displays its String argument to the console,
+// the second does nothing. Using a static import line to import the class into a test
+// program, and demonstrate the conditional compilation effect.
+package access.debugoff;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class Debug {
+    public static void debug(String s) {
+    }
+}
