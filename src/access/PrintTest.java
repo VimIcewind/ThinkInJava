8b6b854c3e24930a2cf2e0bf9a4bff6da966ef19
VimIcewind <VimIcewind@gmail.com>
2018-07-16 15:04:31 +0800

    PrintTest

---
+++ src/access/PrintTest.java
@@ -0,0 +1,22 @@
+//: access/PrintTest.java
+// Uses the static printing methods in Print.java
+package access;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class PrintTest {
+    public static void main(String[] args) {
+        print("Available from now on!");
+        print(100);
+        print(100L);
+        print(3.14159);
+    }
+} /* Output:
+Available from now on!
+100
+100
+3.14159
+*///:~
