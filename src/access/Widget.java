ce71c401a92f2b4a6bad7060d97ddfd1fe9d0541
VimIcewind <VimIcewind@gmail.com>
2018-07-16 20:58:47 +0800

    MakeWidget

---
+++ src/access/Widget.java
@@ -0,0 +1,11 @@
+// access/Widget.java
+package access;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class Widget {
+    public Widget() {
+        System.out.println("Widget()");
+    }
+}
---
+++ src/access/make/MakeWidget.java
@@ -0,0 +1,12 @@
+package access.make;
+
+import access.Widget;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class MakeWidget {
+    public static void main(String[] args) {
+        Widget w = new Widget();
+    }
+}
