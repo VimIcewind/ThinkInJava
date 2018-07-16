10a227d971a4facdd035b8ca48e0a7a6ffc2d242
VimIcewind <VimIcewind@gmail.com>
2018-07-16 14:42:44 +0800

    Collision

---
+++ src/access/Collision.java
@@ -0,0 +1,16 @@
+// access/Colision.java
+// TIJ4 Chapter Access, Exercise 2, page 147
+// Take the code fragments in this section and turn them into a program and
+// verify that collisions do occur.
+package access;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class Collision {
+    public static void main(String[] args) {
+        // Vector v = new Vector(); // ambiquous collision
+        net.mindview.simple.Vector v1 = new net.mindview.simple.Vector();
+        java.util.Vector v2 = new java.util.Vector();
+    }
+}
