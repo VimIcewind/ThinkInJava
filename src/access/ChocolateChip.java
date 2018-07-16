3a7113de59c38696f8647e96534507fda16295ff
VimIcewind <VimIcewind@gmail.com>
2018-07-16 17:17:58 +0800

    ChocolateChip

---
+++ src/access/ChocolateChip.java
@@ -0,0 +1,26 @@
+//: access/ChocolateChip.java
+// Can't use package-access member from another package.
+package access;
+
+import access.dessert.Cookie;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class ChocolateChip extends Cookie {
+    public ChocolateChip() {
+        System.out.println("ChocolateChip constructor");
+    }
+
+    public void chomp() {
+        // bite(); // Can't access bite
+    }
+
+    public static void main(String[] args) {
+        ChocolateChip x = new ChocolateChip();
+        x.chomp();
+    }
+} /* Output:
+Cookie constructor
+ChocolateChip constructor
+*///:~
