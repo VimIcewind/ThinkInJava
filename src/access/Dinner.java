000ede6c13c0a1fef587a4825af041b09a3fd0b5
VimIcewind <VimIcewind@gmail.com>
2018-07-16 16:55:28 +0800

    public

---
+++ src/access/Dinner.java
@@ -0,0 +1,17 @@
+//: access/Dinner.java
+// Uses the library.
+package access;
+
+import access.dessert.Cookie;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class Dinner {
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+        //! x.bite(); // Can't access
+    }
+} /* Output:
+Cookie constructor
+*///:~
---
+++ src/access/dessert/Cookie.java
@@ -0,0 +1,16 @@
+//: access/dessert/Cookie.java
+// Create a library.
+package access.dessert;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class Cookie {
+    public Cookie() {
+        System.out.println("Cookie constructor");
+    }
+
+    void bite() {
+        System.out.println("bite");
+    }
+} ///:~
