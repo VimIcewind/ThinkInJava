55cb16ef327c0b3bcc5b5931ed5765e953ca161e
VimIcewind <VimIcewind@gmail.com>
2018-07-16 20:31:13 +0800

    OrganizedByAccess

---
+++ src/access/OrganizedByAccess.java
@@ -0,0 +1,33 @@
+//: access/OrganizedByAccess.java
+package access;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class OrganizedByAccess {
+    public void pub1() {
+        /* ... */
+    }
+
+    public void pub2() {
+        /* ... */
+    }
+
+    public void pub3() {
+        /* ... */
+    }
+
+    private void priv1() {
+        /* ... */
+    }
+
+    private void priv2() {
+        /* ... */
+    }
+
+    private void priv3() {
+        /* ... */
+    }
+    private int i;
+    // ...
+} ///:~
