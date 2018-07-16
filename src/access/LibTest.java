22dd65b6abf9212845a242d8a1a5e34c2fa9e7bd
VimIcewind <VimIcewind@gmail.com>
2018-07-16 14:25:10 +0800

    LibTest

---
+++ src/access/LibTest.java
@@ -0,0 +1,19 @@
+//: access/LibTest.java
+// Uses the library.
+package access;
+
+import net.mindview.simple.*;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class LibTest {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+        List l = new List();
+    }
+} /* Output:
+net.mindview.simple.Vector
+net.mindview.simple.List
+*///:~
+
