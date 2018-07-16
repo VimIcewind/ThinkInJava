bf5c39f8339e730811eda71c389e969ceb5ce761
VimIcewind <VimIcewind@gmail.com>
2018-07-16 14:19:22 +0800

    simple

---
+++ src/net/mindview/simple/List.java
@@ -0,0 +1,12 @@
+//: net/mindview/simple/List.java
+// Creating a package
+package net.mindview.simple;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class List {
+    public List() {
+        System.out.println("net.mindview.simple.List");
+    }
+} ///:~
---
+++ src/net/mindview/simple/Vector.java
@@ -0,0 +1,12 @@
+//: net/mindview/simple/Vector.java
+// Creating a package.
+package net.mindview.simple;
+
+/**
+ * Created by HWD on 2018-7-16.
+ */
+public class Vector {
+    public Vector() {
+        System.out.println("net.mindview.simple.Vector");
+    }
+} ///:~
