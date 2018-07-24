77ccdd7efb3674e32c7cbe03552eacea7f92eda5
VimIcewind <VimIcewind@gmail.com>
2018-07-24 20:55:08 +0800

    Chess

---
+++ src/reusing/Chess.java
@@ -0,0 +1,34 @@
+//: reusing/Chess.java
+// Inheritance, constructos and arguments.
+package reusing;
+
+/**
+ * Created by HWD on 2018-7-24.
+ */
+class Game {
+    Game(int i) {
+        System.out.println("Game constructor");
+    }
+}
+
+class BoardGame extends Game {
+    BoardGame(int i) {
+        super(i);
+        System.out.println("BoardGame constructor");
+    }
+}
+
+public class Chess extends BoardGame {
+    Chess() {
+        super(11);
+        System.out.println("Chess constructor");
+    }
+
+    public static void main(String[] args) {
+        Chess x = new Chess();
+    }
+} /* Output:
+Game constructor
+BoardGame constructor
+Chess constructor
+*///:~
