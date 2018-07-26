fe7a2b1927e3a716d2895d4805d932a14a8b0f85
VimIcewind <VimIcewind@gmail.com>
2018-07-26 21:02:31 +0800

    Stem Ex10

--- src/reusing/Stem.java
+++ src/reusing/Stem.java
@@ -1,29 +1,27 @@
 // reusing/Stem.java
-// TIJ4 Chapter Reusing, Exercise 9, page 163
-// Create a class called Root that contains an instance of each of the classes
-// (that you also create) named Component1, Component2 and Component3. Derive a
-// class Stem from Root that also contains an instance of each "component." All
-// class should have default constructors that print a message about that class.
+// TIJ4 Chapter Reusing, Exercise 10, page 163
+// Modify the previous exercise so that each class only has non-default
+// constructors.
 package reusing;
 
 /**
  * Created by HWD on 2018-7-26.
  */
 class Component1 {
-    Component1() {
-        System.out.println("Component1()");
+    Component1(byte b) {
+        System.out.println("Component1(byte)");
     }
 }
 
 class Component2 {
-    Component2() {
-        System.out.println("Component2()");
+    Component2(short s) {
+        System.out.println("Component2(short)");
     }
 }
 
 class Component3 {
-    Component3() {
-        System.out.println("Component3()");
+    Component3(int i) {
+        System.out.println("Component3(int)");
     }
 }
 
@@ -32,21 +30,28 @@ class Root {
     Component2 c2root;
     Component3 c3root;
 
-    Root() {
-        System.out.println("Root()");
+    Root(float f) {
+        c1root = new Component1((byte)0);
+        c2root = new Component2((short)0);
+        c3root = new Component3(0);
+        System.out.println("Root(float)");
     }
 }
 
 public class Stem extends Root {
-    Component1 c1stem;
-    Component2 c2stem;
-    Component3 c3stem;
+    Component1 c1stem10;
+    Component2 c2stem10;
+    Component3 c3stem10;
 
-    Stem() {
-        System.out.println("Stem()");
+    Stem(double d) {
+        super(2.78f);
+        c1stem10 = new Component1((byte)1);
+        c2stem10 = new Component2((short)1);
+        c3stem10 = new Component3(1);
+        System.out.println("Stem(double)");
     }
 
     public static void main(String[] args) {
-        Stem s = new Stem();
+        Stem s = new Stem(2.78);
     }
 }
