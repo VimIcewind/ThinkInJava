23042280911db6ab4ba054a171822410ce06212c
VimIcewind <VimIcewind@gmail.com>
2018-08-05 21:20:19 +0800

    Difference

---
+++ src/reusing/Difference.java
@@ -0,0 +1,44 @@
+// TIJ4 Chapter Reusing, Exercise 18, page 175
+// Create a class with a static final field and a final filed and demonstrate
+// the difference between the two.
+package reusing;
+
+import java.util.Random;
+
+/**
+ * Created by HWD on 2018-8-5.
+ */
+class Test {
+    Test() {
+        System.out.println("Test()");
+    }
+}
+
+public class Difference {
+    private String name;
+
+    public Difference(String s) {
+        name = s;
+    }
+
+    static final Test sft = new Test(); // constant reference address
+    private final Test ft = new Test();
+    static final String SFS = "static final"; // class constant
+    private final String fs = "final";
+    private static Random rand = new Random();
+    static final int SFI = rand.nextInt(); // class constant
+    private final int fi = rand.nextInt();
+
+    public String toString() {
+        return (name + ": " + sft + ", " + ft + ", " + SFS + ", " + fs + ", " + SFI + ", " + fi);
+    }
+
+    public static void main(String[] args) {
+        Difference d1 = new Difference("d1");
+        Difference d2 = new Difference("d2");
+        Difference d3 = new Difference("d3");
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+    }
+}
