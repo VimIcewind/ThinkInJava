7db8f295bdd70690804c52f95f0c248c8a4113d5
VimIcewind <VimIcewind@gmail.com>
2018-08-13 21:27:35 +0800

    Cycle

---
+++ src/polymorhpism/biking/Biking.java
@@ -0,0 +1,62 @@
+// polymorphism/biking/Biking.java
+// TIJ4 Chapter Polymorphism, Exercise 1, page 183
+/* Create a Cycle class, with subclasses Unicycle, Bicycle and Tricycle.
+* Demonstrate that an instance of each type can be upcaste to Cycle via a ride()
+* method.
+*/
+
+package polymorhpism.biking;
+
+/**
+ * Created by HWD on 2018-8-13.
+ */
+class Cycle {
+    private String name = "Cycle";
+
+    public static void travel(Cycle c) {
+        System.out.println("Cycle.ride() " + c);
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
+
+class Unicycle extends Cycle {
+    private String name = "Unicycle";
+
+    public String toString() {
+        return this.name;
+    }
+}
+
+class Bicycle extends Cycle {
+    private String name = "Bicycle";
+
+    public String toString() {
+        return this.name;
+    }
+}
+
+class Tricycle extends Cycle {
+    private String name = "Tricycle";
+
+    public String toString() {
+        return this.name;
+    }
+}
+
+public class Biking {
+    public static void ride(Cycle c) {
+        c.travel(c);
+    }
+
+    public static void main(String[] args) {
+        Unicycle u = new Unicycle();
+        Bicycle b = new Bicycle();
+        Tricycle t = new Tricycle();
+        ride(u);
+        ride(b);
+        ride(t);
+    }
+}
