acd94e3af36c140e58dab95e9dc9e8f3660978ad
VimIcewind <VimIcewind@gmail.com>
2018-08-20 22:20:20 +0800

    Exercise 15

--- src/polymorphism/PolyConstructors.java
+++ src/polymorphism/PolyConstructors.java
@@ -33,13 +33,35 @@ class RoundGlyph extends Glyph {
     }
 }
 
+class RectangularGlyph extends Glyph {
+    private int length = 2;
+    private int width = 4;
+
+    RectangularGlyph(int l, int w) {
+        length = l;
+        width = w;
+        print("RectangularGlyph.RectangularGlyph(), length = "
+                + length + ", width" + width);
+    }
+
+    void draw() {
+        print("RectangularGlyph.draw(), length = " + length
+                + ", with = " + width);
+    }
+}
+
 public class PolyConstructors {
     public static void main(String[] args) {
         new RoundGlyph(5);
+        new RectangularGlyph(3, 6);
     }
 } /* Output:
 Glyph() before draw()
 RoundGlyph.draw(), radius = 0
 Glyph() after draw()
 RoundGlyph.RoundGlyph(), radius = 5
+Glyph() before draw()
+RectangularGlyph.draw(), length = 0, with = 0
+Glyph() after draw()
+RectangularGlyph.RectangularGlyph(), length = 3, width6
 *///:~
