409738a6e65d19f8098c398c88f7929035b3eada
VimIcewind <VimIcewind@gmail.com>
2018-09-05 20:50:02 +0800

    DotNew

---
+++ src/innerclasses/DotNew.java
@@ -0,0 +1,14 @@
+//: innerclasses/DotNew.java
+// Creating an inner class directly using the .new syntax.
+package innerclasses;
+
+public class DotNew {
+    public class Inner {
+
+    }
+
+    public static void main(String[] args) {
+        DotNew dn = new DotNew();
+        DotNew.Inner dni = dn.new Inner();
+    }
+} ///:~
