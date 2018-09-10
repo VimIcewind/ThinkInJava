9fa8e5942e7472c90c510d0d6c18fd121a157c48
VimIcewind <VimIcewind@gmail.com>
2018-09-10 21:43:54 +0800

    AnonymousConstructor

---
+++ src/innerclasses/AnonymousConstructor.java
@@ -0,0 +1,37 @@
+//: innerclasses/AnonymousConstructor.java
+// Creating a constructor for an anonymous inner class.
+package innerclasses;
+
+import static net.mindview.util.Print.print;
+
+abstract class Base {
+    public Base(int i) {
+        print("Base constructor, i = " + i);
+    }
+
+    public abstract void f();
+}
+
+public class AnonymousConstructor {
+    public static Base getBase(int i) {
+        return new Base(i) {
+            {
+                print("Inside instance initializer");
+            }
+
+            @Override
+            public void f() {
+                print("In anonymous f()");
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        Base base = getBase(47);
+        base.f();
+    }
+} /* Output:
+Base constructor, i = 47
+Inside instance initializer
+In anonymous f()
+*///:~
