8949c89a494b6f61e12819e8de22ac2c7bf8f65f
VimIcewind <VimIcewind@gmail.com>
2018-09-10 21:29:51 +0800

    Parcel8

---
+++ src/innerclasses/Wrapping.java
@@ -0,0 +1,14 @@
+//: innerclasses/Wrapping.java
+package innerclasses;
+
+public class Wrapping {
+    private int i;
+
+    public Wrapping(int x) {
+        i = x;
+    }
+
+    public int value() {
+        return i;
+    }
+} ///:~
