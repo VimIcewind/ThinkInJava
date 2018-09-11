d06aaf73b44f3400653e1c7f8226f904426e48d9
VimIcewind <VimIcewind@gmail.com>
2018-09-11 21:25:01 +0800

    Ex15

---
+++ src/innerclasses/ex15/Ex15.java
@@ -0,0 +1,33 @@
+// innerclasses/ex15/Ex15.java
+// TIJ4 Chapter Innerclasses, Exercise 15, page 232
+/* Create a class with a non-default constructor and no default constructor.
+ * Create a second class that has a method that returns a reference to an
+ * object of the first class. Create the object that you return by making an
+ * anonymous inner class that inherits from the first class.
+ */
+package innerclasses.ex15;
+
+class One {
+    private String s;
+
+    One(String s) {
+        this.s = s;
+    }
+
+    public String showS() {
+        return s;
+    }
+}
+
+public class Ex15 {
+    public One makeOne(String s) {
+        return new One(s) {
+
+        };
+    }
+
+    public static void main(String[] args) {
+        Ex15 x = new Ex15();
+        System.out.println(x.makeOne("hi").showS());
+    }
+}
