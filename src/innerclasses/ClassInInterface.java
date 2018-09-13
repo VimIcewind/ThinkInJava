174bec1e5d5ee8b6a41d2495574eae6a6468dff4
VimIcewind <VimIcewind@gmail.com>
2018-09-13 20:48:40 +0800

    ClassInInterface

---
+++ src/innerclasses/ClassInInterface.java
@@ -0,0 +1,20 @@
+//: innerclasses/ClassInInterface.java
+// {main: ClassInInterface$Test}
+package innerclasses;
+
+public interface ClassInInterface {
+    void howdy();
+
+    class Test implements ClassInInterface {
+        @Override
+        public void howdy() {
+            System.out.println("Howdy!");
+        }
+
+        public static void main(String[] args) {
+            new Test().howdy();
+        }
+    }
+} /* Output:
+Howdy!
+*///:~
