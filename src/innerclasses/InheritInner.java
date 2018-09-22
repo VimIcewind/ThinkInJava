156f5308cd8487eb857929d510cafd9f99478130
VimIcewind <VimIcewind@gmail.com>
2018-09-22 21:30:53 +0800

    InheritInner

---
+++ src/innerclasses/InheritInner.java
@@ -0,0 +1,22 @@
+//: innerclasses/InheritInner.java
+// Inheriting an inner class.
+package innerclasses;
+
+class WithInner {
+    class Inner {
+
+    }
+}
+
+public class InheritInner extends WithInner.Inner {
+    //! InheritInner() {} // Won't compile
+
+    public InheritInner(WithInner wi) {
+        wi.super();
+    }
+
+    public static void main(String[] args) {
+        WithInner wi = new WithInner();
+        InheritInner ii = new InheritInner(wi);
+    }
+}
