56dda9b250b590ced1258386484f2b400f59dac2
VimIcewind <VimIcewind@gmail.com>
2018-09-26 21:03:49 +0800

    Ex2

---
+++ src/holding/ex2/Ex2.java
@@ -0,0 +1,18 @@
+// holding/ex2/Ex2.java
+// TIJ4 Chapter Holding, Exercise 2, page 253
+// Modify SimpleCollection.java to use a Set for c.
+package holding.ex2;
+
+import java.util.*;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Set<Integer> c = new HashSet<Integer>();
+        for (int i = 0; i < 10; i++) {
+            c.add(i); // Autoboxing
+        }
+        for (Integer i : c) {
+            System.out.print(i + ", ");
+        }
+    }
+}
