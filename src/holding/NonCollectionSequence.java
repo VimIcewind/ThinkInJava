377203431abc8773bd366fe2166ebb90ea14ebef
VimIcewind <VimIcewind@gmail.com>
2019-03-08 16:24:43 +0800

    NonCollectionSequence

---
+++ src/holding/NonCollectionSequence.java
@@ -0,0 +1,43 @@
+//: holding/NonCollectionSequence.java
+package holding;
+
+import typeinfo.pets.*;
+
+import java.util.Iterator;
+
+/**
+ * Created by HWD on 2019/3/8 16:19
+ */
+class PetSequence {
+    protected Pet[] pets = Pets.createArray(8);
+}
+
+public class NonCollectionSequence extends PetSequence {
+    public Iterator<Pet> iterator() {
+        return new Iterator<Pet>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < pets.length;
+            }
+
+            @Override
+            public Pet next() {
+                return pets[index++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        NonCollectionSequence nc = new NonCollectionSequence();
+        InterfaceVsIterator.display(nc.iterator());
+    }
+} /* Output:
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+*///:~
