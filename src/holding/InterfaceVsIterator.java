5702865876bd6a4995f64be05e5df9d8d8a81151
VimIcewind <VimIcewind@gmail.com>
2019-03-08 16:04:31 +0800

    InterfaceVsIterator

---
+++ src/holding/InterfaceVsIterator.java
@@ -0,0 +1,54 @@
+//: holding/InterfaceVsIterator.java
+package holding;
+
+import typeinfo.pets.*;
+
+import java.util.*;
+
+/**
+ * Created by HWD on 2019/3/8 13:49
+ */
+public class InterfaceVsIterator {
+    public static void display(Iterator<Pet> it) {
+        while (it.hasNext()) {
+            Pet p = it.next();
+            System.out.print(p.id() + ":" + p + " ");
+        }
+        System.out.println();
+    }
+
+    public static void display(Collection<Pet> pets) {
+        for (Pet p : pets) {
+            System.out.print(p.id() + ":" + p + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        List<Pet> petList = Pets.arrayList(8);
+        Set<Pet> petSet = new HashSet<Pet>(petList);
+        Map<String, Pet> petMap = new LinkedHashMap<String, Pet>();
+        String[] names = ("Ralph, Eric, Robin, lacey, " +
+                "Britney, Sam, Spot, Fluffy").split(", ");
+        for (int i = 0; i < names.length; i++) {
+            petMap.put(names[i], petList.get(i));
+        }
+        display(petList);
+        display(petSet);
+        display(petList.iterator());
+        display(petSet.iterator());
+        System.out.println(petMap);
+        System.out.println(petMap.keySet());
+        display(petMap.values());
+        display(petMap.values().iterator());
+    }
+} /* Output:
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+{Ralph=Rat, Eric=Manx, Robin=Cymric, lacey=Mutt, Britney=Pug, Sam=Cymric, Spot=Pug, Fluffy=Manx}
+[Ralph, Eric, Robin, lacey, Britney, Sam, Spot, Fluffy]
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+*///:~
