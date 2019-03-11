7638a6ad53af2bf5aeaacf010e7d0ba084e08dd1
VimIcewind <VimIcewind@gmail.com>
2019-03-11 09:50:32 +0800

    Ex30

---
+++ src/holding/ex30/CollectionSequence.java
@@ -0,0 +1,109 @@
+//: holding/ex30/CollectionSequence.java
+package holding.ex30;
+
+import holding.InterfaceVsIterator;
+import typeinfo.pets.*;
+
+import java.util.*;
+
+/**
+ * Created by HWD on 2019/3/11 09:38
+ */
+public class CollectionSequence implements Collection {
+    private Pet[] pets = Pets.createArray(8);
+
+    @Override
+    public int size() {
+        return pets.length;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (this.size() == 0) ? true : false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<Pet> iterator() {
+        return new Iterator<Pet>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < pets.length;
+            }
+
+            @Override
+            public Pet next() {
+                return pets[index++];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    @Override
+    public Object[] toArray() {
+        return pets;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        if (this.size() != 0) {
+            for (Pet p : pets) {
+                p = null;
+            }
+        }
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return pets;
+    }
+
+    public static void main(String[] args) {
+        CollectionSequence c = new CollectionSequence();
+        InterfaceVsIterator.display(c);
+        InterfaceVsIterator.display(c.iterator());
+    }
+} /* Output:
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+*///:~
