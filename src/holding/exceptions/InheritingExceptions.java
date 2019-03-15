08a2738b1335d9afbcea099ed6d6950a139e8b04
VimIcewind <VimIcewind@gmail.com>
2019-03-15 21:42:37 +0800

    InheritingExceptions

---
+++ src/holding/exceptions/InheritingExceptions.java
@@ -0,0 +1,29 @@
+//: exceptions/InheritingExceptions.java
+// Creating your own exceptions.
+package holding.exceptions;
+
+/**
+ * Created by HWD on 2019/3/15 21:37
+ */
+class SimpleException extends Exception {
+
+}
+
+public class InheritingExceptions {
+    public void f() throws SimpleException {
+        System.out.println("Throw SimpleException from f()");
+        throw new SimpleException();
+    }
+
+    public static void main(String[] args) {
+        InheritingExceptions sed = new InheritingExceptions();
+        try {
+            sed.f();
+        } catch(SimpleException e) {
+            System.out.println("Caught it!");
+        }
+    }
+} /* Output:
+Throw SimpleException from f()
+Caught it!
+*///:~
